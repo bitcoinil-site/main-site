6f7826ca5daf629bacd993083d2ca3f4cd352198
Eli Sklar <eli.sklar@gmail.com>
2022-08-23 11:57:20 +0300

    Update innovation page

--- apps/bitcoin-il/src/components/InnovationBody.tsx
+++ apps/bitcoin-il/src/components/InnovationBody.tsx
@@ -1,13 +1,44 @@
+import { Col, Grid, Row } from 'antd'
 import * as React from 'react'
+import { useIntl } from 'react-intl'
 import styled from 'styled-components'
 
 import { innovationCards } from '../data/InnovationData'
 import { InnovationBodyProps } from '../utils/interfaces'
 import CardsDisplay from './CardsDisplay'
+import { FormattedMessage } from './FormattedMessageWithHover'
 
 const InnovationBody: React.FC<InnovationBodyProps> = ({}) => {
+  const intl = useIntl()
+
+  const paragraphs = React.useMemo(
+    () =>
+      Object.entries(intl.messages)
+        .filter(([k]) => k.match(/^innovation\.why\-clone.paragraph\.[0-9]+/))
+        .sort(([a], [b]) => (a > b ? 1 : -1)),
+    [intl.messages]
+  )
+
   return (
     <StyledInnovationBody id="IndividualsBody">
+      <Row>
+        <Col span={6} />
+        <Col span={12}>
+          <h2>
+            <FormattedMessage
+              id="innovation.why-clone.title"
+              defaultMessage="Why Clone?"
+            />
+          </h2>
+          {paragraphs.length > 0 &&
+            paragraphs.map(([key, value]) => (
+              <p key={key}>
+                <FormattedMessage id={key} defaultMessage={value} />
+              </p>
+            ))}
+        </Col>
+      </Row>
+      <hr />
       <CardsDisplay cards={innovationCards} />
     </StyledInnovationBody>
   )
